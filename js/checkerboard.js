01a45396bc66d7013b4ba704fa654c05df24d79a
lusterofgem <lusterofgem@gmail.com>
2022-10-18 12:01:31 +0800

    fix layout

--- js/checkerboard.js
+++ js/checkerboard.js
@@ -1,19 +1,28 @@
-//畫布畫筆
-var chess = document.getElementById("chess");
-var context = chess.getContext("2d"); //context可以看作畫筆
-context.strokeStyle="#bfbfbf";              //畫筆的顏色
+document.addEventListener("DOMContentLoaded", () => {
+    //畫布畫筆
+    let checkerboard = document.getElementById("checkerboard");
+    let context = checkerboard.getContext("2d"); //context可以看作畫筆
+    context.strokeStyle="#bfbfbf";              //畫筆的顏色
 
-//載入棋盤
-window.onload = function(){               //頁面載入完成事件
-    for(var i=0;i<15;i++){
-    
-    context.moveTo(15,15+30*i);          //橫線（x，y）起始點
-    context.lineTo(435,15+30*i);           //橫線（x，y）終止點
-    context.stroke();                              //畫一條線
-    
-    context.moveTo(15+30*i,15);           //豎線
-    context.lineTo(15+30*i,435);
-    context.stroke();
+    // //載入棋盤
+    // for(var i=0;i<15;i++) {
+    //     context.moveTo(15,15+30*i);          //橫線（x，y）起始點
+    //     context.lineTo(435,15+30*i);           //橫線（x，y）終止點
+    //     context.stroke();                              //畫一條線
+    //
+    //     context.moveTo(15+30*i,15);           //豎線
+    //     context.lineTo(15+30*i,435);
+    //     context.stroke();
+    // }
+
+    //載入棋盤
+    for(var i=0;i<15;i++) {
+        context.moveTo(0,30*i);          //橫線（x，y）起始點
+        context.lineTo(420,30*i);           //橫線（x，y）終止點
+        context.stroke();                              //畫一條線
+
+        context.moveTo(30*i,0);           //豎線
+        context.lineTo(30*i,420);
+        context.stroke();
     }
-    
-}
+});
